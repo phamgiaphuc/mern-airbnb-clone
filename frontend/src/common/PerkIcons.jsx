f0aa3cf8aab2b0e9b844a0d37010d92375c163d2
Acus <acus1304pgphcm@gmail.com>
2024-02-08 13:53:53 +0700

    Phuc | Complete the project

---
+++ frontend/src/common/PerkIcons.jsx
@@ -0,0 +1,34 @@
+import { WifiIcon, TruckIcon, TvIcon, HandThumbUpIcon, ArrowLeftEndOnRectangleIcon, RadioIcon } from "@heroicons/react/24/outline"
+
+export const icons = [
+  {
+    perk: 'wifi',
+    name: 'Wifi',
+    icon: WifiIcon
+  },
+  {
+    perk: 'parking',
+    name: 'Free parking spot',
+    icon: TruckIcon
+  },
+  {
+    perk: 'tv',
+    name: 'TV',
+    icon: TvIcon
+  },
+  {
+    perk: 'radio',
+    name: 'Radio',
+    icon: RadioIcon
+  },
+  {
+    perk: 'pets',
+    name: 'Pets',
+    icon: HandThumbUpIcon
+  },
+  {
+    perk: 'entrance',
+    name: 'Private entrance',
+    icon: ArrowLeftEndOnRectangleIcon
+  }
+]
